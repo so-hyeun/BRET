01f1cdb730586079b7941260b789b42e984b0d1a
miruware <webby1815@gmail.com>
2020-09-07 12:05:44 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template, url_for
+from flask_bootstrap import Bootstrap
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/Chemical_Disease.html')
+def ChemDis():
+    return render_template('Chemical_Disease.html')
+
+@app.route('/Drug_Drug.html')
+def DrugDrug():
+    return render_template('Drug_Drug.html')
+
+@app.route('/Gene_Disease.html')
+def GeneDise():
+    return render_template('Gene_Disease.html')
+
+
+if __name__ == "__main__":
+    app.run()
