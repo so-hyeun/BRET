52f4cc297d87aea9ce5c8f63896834bfaa4905c2
sonsuhyune <s3267s@naver.com>
2020-09-08 11:27:25 +0900

    file upload and receive result file

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 from flask_bootstrap import Bootstrap
 from flask import Flask, request, render_template, url_for
 
-
+from predict_temp import chem_dis_func,drug2_func,gene_dis_func
 from werkzeug.utils import redirect, secure_filename
 
-UPLOAD_DIR = "static/result_file/"
+UPLOAD_DIR = "static/result/"
 
 app = Flask(__name__)
 app.config['UPLOAD_DIR'] = UPLOAD_DIR
@@ -20,9 +20,20 @@ def home():
 def index():
     return render_template('index.html')
 
-@app.route('/Chemical_Disease.html')
+@app.route('/Chemical_Disease.html',methods=['GET','POST'])
 def ChemDis():
-    return render_template('Chemical_Disease.html')
+    if request.method == 'GET':
+        return render_template('Chemical_Disease.html')
+    elif request.method == 'POST':
+        f = request.files['fileToUpload']
+        fname = secure_filename(f.filename)
+        path = app.config['UPLOAD_DIR']+fname
+        f.save(path)
+        result = chem_dis_func("file ") #이 함수에 사용자가 upload한 file
+
+        return render_template('Chemical_Disease.html', model_result=result)
+
+    
 
 @app.route('/Drug_Drug.html', methods=['GET','POST'])
 def DrugDrug():
@@ -33,15 +44,30 @@ def DrugDrug():
         fname = secure_filename(f.filename)
         path = app.config['UPLOAD_DIR']+fname
         f.save(path)
-        return 'file uploaded successfully'
+        result = drug2_func("file ") #이 함수에 사용자가 upload한 file
 
-@app.route('/Gene_Disease.html')
+        return render_template('Drug_Drug.html', model_result=result)
+
+
+
+@app.route('/Gene_Disease.html', methods=['GET','POST'])
 def GeneDise():
-    return render_template('Gene_Disease.html')
+    if request.method == 'GET':
+        return render_template('Gene_Disease.html')
+    elif request.method == 'POST':
+        f = request.files['fileToUpload']
+        fname = secure_filename(f.filename)
+        path = app.config['UPLOAD_DIR']+fname
+        f.save(path)
+        result = gene_dis_func("file ") #이 함수에 사용자가 upload한 file
+
+        return render_template('Gene_Disease.html', model_result=result)
 
 @app.route('/contact.html')
 def contact():
     return render_template('contact.html')
+
+
 
 
 if __name__ == "__main__":
---
+++ predict_temp.py
@@ -0,0 +1,8 @@
+def chem_dis_func(f):
+    return f+"Chemprot_disease_result_json"
+
+def drug2_func(f):
+    return f+"Drug_Drug_result_json"
+
+def gene_dis_func(f):
+    return f+"Gene_Disease_result_json"
